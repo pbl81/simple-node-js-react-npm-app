efcae37fcc8c7d9ecb5b64db9a9ae229ececa92b
pbl81 <88390080+pbl81@users.noreply.github.com>
2024-05-20 08:24:52 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,7 +4,7 @@ echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
+echo 'the best performance..'
 set -x
 npm run build
 set +x
